4b675edc1b63962116ae857cbc3b560c288d8f05
Azkha Nazzala <azkhanazzala@gmail.com>
2026-09-04 12:07:59 +0700

    feat: konfigurasi supabase client dan inisialisasi aplikasi

---
+++ matcha_app/lib/core/config/supabase_config.dart
@@ -0,0 +1,6 @@
+class SupabaseConfig {
+  SupabaseConfig._();
+
+  static const String url = 'https://fdkwxdeqbrlrqsuifyqw.supabase.co';
+  static const String anonKey = 'sb_publishable_2JULl0wOTWvjx7mxB9moiA_AxAoI9u3';
+}
