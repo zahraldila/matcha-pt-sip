c9e9f1941edd8972191679fe623d33d993f17e69
Azkha Nazzala <azkhanazzala@gmail.com>
2026-09-21 10:06:21 +0700

    feat(config): tambahkan konfigurasi supabase dan dependensi bcrypt

--- matcha_app/lib/core/config/supabase_config.dart
+++ matcha_app/lib/core/config/supabase_config.dart
@@ -1,6 +1,6 @@
 class SupabaseConfig {
   SupabaseConfig._();
 
-  static const String url = 'https://fdkwxdeqbrlrqsuifyqw.supabase.co';
-  static const String anonKey = 'sb_publishable_2JULl0wOTWvjx7mxB9moiA_AxAoI9u3';
+  static const String url = 'https://xkyneehswdqkdgzodwdc.supabase.co';
+  static const String anonKey = 'sb_secret_cluCReIwMQQjzo4_ViZc6g_rk6cQFSa';
 }
